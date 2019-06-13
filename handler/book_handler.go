1cc9273544afc3d395266d85ad88a6fd7def8bea
yyh-gl <yhonda.95.gl@gmail.com>
2019-06-13 17:41:28 +0900

    ハンドラー修正

--- handler/book_handler.go
+++ handler/book_handler.go
@@ -1,11 +1,25 @@
 package handler
 
 import (
-	"fmt"
+	"encoding/json"
 	"github.com/julienschmidt/httprouter"
+	"github.com/yyh-gl/go-api-server-by-ddd/domain/model"
+	"github.com/yyh-gl/go-api-server-by-ddd/usecase"
 	"net/http"
 )
 
-func BookIndex(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
-	fmt.Fprint(w, "Welcome!\n")
+func BookIndex(w http.ResponseWriter, _ *http.Request, _ httprouter.Params) {
+	var books []*model.Book
+	var err error
+	books, err = usecase.IBookUsecase(usecase.BookUsecase{}).GetAll()
+	if err != nil {
+		// TODO: エラーレスポンスを返す
+		panic(err)
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	if err = json.NewEncoder(w).Encode(books); err != nil {
+		// TODO: エラーレスポンスを返す
+		panic(err)
+	}
 }
