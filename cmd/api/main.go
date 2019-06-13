f390234dd9f4aa61945dd8a1a70db0b722e8c7bd
yyh-gl <yhonda.95.gl@gmail.com>
2019-06-13 23:45:45 +0900

    ルーターの位置を修正

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -5,12 +5,12 @@ import (
 	"log"
 	"net/http"
 
-	"github.com/yyh-gl/api-server-with-go-kit-and-ddd/infra"
+	"github.com/yyh-gl/go-api-server-by-ddd/handler"
 )
 
 func main() {
 	fmt.Println("========================")
 	fmt.Println("Server Start >> http://localhost:3000")
 	fmt.Println("========================")
-	log.Fatal(http.ListenAndServe(":3000", infra.Router))
+	log.Fatal(http.ListenAndServe(":3000", handler.Router))
 }
